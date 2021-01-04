254d3f2120365d9b53f0cb34f033936a7d34ef86
scruic <Hongchao_Qin@saic-gm.com>
2021-01-04 13:34:24 +0800

    引入antd 和路由

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React ,{Component} from 'react'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import Login from './pages/login'
+import Admin from './pages/admin'
+
 
 export default class App extends Component
 {
     render(){
-        return <div>App.....</div>
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/' component={Admin}></Route>
+                    <Route path='/login' component={Login}></Route>
+                </Switch>
+            </BrowserRouter>
+        )
     }
 }
